5bf50fe5a58c15a21186e47ea2874e6e6aa40006
Ulukele <arkhip2002@gmail.com>
2022-07-20 20:54:11 +0700

    set params from env

--- frontend/improved-todo/replace_api_url.sh
+++ frontend/improved-todo/replace_api_url.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,API_BASE_URL,'"$API_BASE_URL"',g' {} \;
+find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,_API_BASE_URL,'"$API_BASE_URL"',g' {} \;
 nginx -g "daemon off;"
